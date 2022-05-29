cd5670fec872802a105b6f01a0f7eea1a8d99814
Mikhail <mishanyaveselyi@gmail.com>
2022-05-29 16:06:06 -0400

    Set up router

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors"
+import postRoutes from "./routes/posts.js";
+
+const app = express()
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://mikhail1990:PASTPERFECT123@cluster0.10eaf.mongodb.net/?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => {console.log(`Server runs on port ${PORT}`)})
+  .catch((error) => console.log(error.message));
