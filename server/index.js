e8c07347fdbafb67e04c0d1b5b9e85b89f31d93f
Mikhail <mishanyaveselyi@gmail.com>
2022-06-04 14:39:22 -0400

    Fix mongoose.connect function

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors"
-import postRoutes from "./routes/posts.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-const app = express()
+import postRoutes from './routes/posts.js';
 
-app.use('/posts', postRoutes)
+const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
-app.use(cors())
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://mikhail1990:PASTPERFECT123@cluster0.10eaf.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT|| 5000;
 
-const PORT = process.env.PORT || 5000
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.connect(CONNECTION_URL)
-  .then(() => {console.log(`Server runs on port ${PORT}`)})
-  .catch((error) => console.log(error.message));
+// mongoose.set('useFindAndModify', false);
