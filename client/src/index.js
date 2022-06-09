20687afbee379425cd273133c7f62ef8e9b64424
Mikhail <mishanyaveselyi@gmail.com>
2022-06-08 23:36:11 -0400

    Set posts reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { configureStore, applyMidleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 
 import App from "./App"
+
+const store = configureStore(reducers, compose(applyMidleware(thunk)))
 
 ReactDOM.render(<App />, document.getElementById('root'))
