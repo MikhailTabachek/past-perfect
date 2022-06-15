7063f4deef9705cd147c50ec258a812db234dd8a
Mikhail <mishanyaveselyi@gmail.com>
2022-06-15 16:34:01 -0400

    Changed server enviroment settings

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+import 'dotenv/config.js'
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 
 import postRoutes from './routes/posts.js';
 
@@ -14,11 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://mikhail1990:PASTPERFECT123@cluster0.10eaf.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.DATABASE_URL
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
-// mongoose.set('useFindAndModify', false);
