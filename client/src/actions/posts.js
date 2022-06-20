463a627e7582c15997c3e9039ca70b863717239a
Mikhail <mishanyaveselyi@gmail.com>
2022-06-20 17:22:01 -0400

    Set CreatePost API request function

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -11,3 +11,13 @@ export const getPosts = () => async(dispatch) => {
   }
 
 }
+
+//function to send a post request to the server
+export const createPost = (post) => async(dispatch) => {
+  try {
+    const { data } = await api.createPost(post)
+    dispatch({ type: 'CREATE', payload:data })
+  } catch (error) {
+    console.log(error)
+  }
+}
