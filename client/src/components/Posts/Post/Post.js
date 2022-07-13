a8ba0d806f8e62f1075ce514ae12aeb8c5fea3ac
Mikhail <mishanyaveselyi@gmail.com>
2022-07-13 17:42:04 -0400

    Create index.css

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -13,9 +13,10 @@ const Post = ( {post} ) => {
   return(
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
-      <div className={classes.overlay}></div>
+      <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
       <div className={classes.overlay2}>
         <Button style={{color: 'white'}} size='small' onClick={() => {}}>
           <MoreHorizIcon fontSize="default"/>
@@ -23,7 +24,6 @@ const Post = ( {post} ) => {
       </div>
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
-
       </div>
       <CardContent>
         <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
