e3d271604a342f6f267acc755860baa6172ad025
Mikhail <mishanyaveselyi@gmail.com>
2022-08-28 22:18:15 -0400

    add Like reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,9 +2,10 @@ export default (posts = [], action) => {
   switch (action.type) {
     case 'DELETE': 
       return posts.filter(( post ) => post._id !== action.payload)
-    case 'UPDATE':
-      return posts.map((post) => post._id === action.payload._id ? action.payload : post)
-    case 'FETCH_ALL':
+      case 'UPDATE':
+        case 'LIKE':
+        return posts.map((post) => post._id === action.payload._id ? action.payload : post)
+      case 'FETCH_ALL':
       return action.payload
     case 'CREATE':
       return [...posts,action.payload];
